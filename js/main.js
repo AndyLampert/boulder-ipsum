8f8886d0880dafef5dbf082a55bb80fdb1352bbf
AndyLampert <lamperta@gmail.com>
2015-02-02 17:20:59 -0700

    add line breaks

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 
-var items = ['Boulder', 'flatiron', 'bouldering', 'Avery', 'up the canyon', 'Bolder Boulder', 'Bodarging', 'sit on the grass', 'organic', 'GMO-free', 'triathlon', 'marathon', 'ironman', 'pearl street mall', '29th street', 'b2b', 'microbreweries', 'homebrowing', 'b2b home grow', 'indica', 'sativa', 'home grown b2b', 'hybrid', 'percalator', 'carborator', 'carb', 'reiki', 'reiki master', 'naropa', 'dragonfly', 'cosmos', 'abo\'s', 'illegal pete\'s', 'smelly deli', 'buffalos', 'CU Buffs', 'mountain sun', 'the sun', 'the southern sun', 'under the sun', 'conor\'s', 'TechStars', 'Tesla', 'Boulder Baked', 'ncar', 'mount sanitas', 'drive up to Ned', 'tubing', 'canyoning', 'Chautauqua', 'eTown Hall', 'The Fox', 'Boulder Theater', 'dead leaf', 'Naked Pumpkin Run', 'the cruise', 'thursday night cruise', 'banjo billy\'s', 'yoga', 'goss gross', 'the nexus', 'Arapahoe', 'Baseline', 'North Boulder', 'Table Mesa', 'Table Mesa Park N Ride', 'The Diagonal'
+var items = ['Boulder', 'rolfing', 'Liquor Mart', 'The Spot', 'flatiron', 'bouldering', 'Avery', 'up the canyon', 'Bolder Boulder', 'Bodarging', 'sit on the grass', 'organic', 'ganja', '420', 'GMO-free', 'triathlon', 'marathon', 'ironman', 'pearl street mall', '29th street', 'b2b', 'microbreweries', 'homebrowing', 'b2b home grow', 'indica', 'sativa', 'home grown b2b', 'hybrid', 'percalator', 'carborator', 'carb', 'reiki', 'reiki master', 'naropa', 'dragonfly', 'cosmos', 'abo\'s', 'illegal pete\'s', 'smelly deli', 'buffalos', 'CU Buffs', 'mountain sun', 'the sun', 'the southern sun', 'under the sun', 'conor\'s', 'TechStars', 'Tesla', 'Boulder Baked', 'ncar', 'mount sanitas', 'drive up to Ned', 'tubing', 'canyoning', 'Chautauqua', 'eTown Hall', 'The Fox', 'Boulder Theater', 'dead leaf', 'Naked Pumpkin Run', 'the cruise', 'thursday night cruise', 'banjo billy\'s', 'yoga', 'tai chi', 'goss gross', 'the nexus', 'Arapahoe', 'Baseline', 'North Boulder', 'Table Mesa', 'Table Mesa Park N Ride', 'The Diagonal', 'Spartan Race', 'balls to the walls', 'hemp', 'gluten free', 'Snarf\'s', 'bike lanes', 'farmer\'s market', 'Wonderland Lake', 'Coconut oil is amazing'
 
 ];
 
 // have it put a period after a random number of words like that and then capitalize the first letter of the new sentence
-noCommaList = items.join(' ');
-noCommaList[4];
-
 var dump = $('.dump');
 
 $('.show-results-btn').click(function(){
-	paragraphCount = $('#paragraphCount').val();
-	var shuffledItems = _.shuffle(items);
+	// clear the DOM on click
+	dump.empty()
+	var paragraphCount = $('#paragraphCount').val();
 
-	// Math.floor(Math.random() * 5) + 3;
-	var shuffledWithComma = shuffledItems.map(function(str, index) {
+	// Add a comma to all items
+	var withComma = items.map(function(str, index) {
 		if (index % 4 === 0) {
 			return str + ', ';
 		} else {
@@ -22,7 +20,14 @@ $('.show-results-btn').click(function(){
 		}
 	});
 
+	// paragraphCount times, shuffle the 
 	_.times(paragraphCount, function(){
+		var shuffledWithComma = _.shuffle(withComma);
+		// debugger;
+		// adding an opening div before the array
+		shuffledWithComma.unshift('<div class="dump-item"' + '<br>' + '<br>');
+		// adding an opening div after the array
+		shuffledWithComma.push('</div>');
 		dump.append(shuffledWithComma);
 	})
 });
