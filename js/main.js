727c17a2117c11a0688eccdc0bf1e7b120d64d65
AndyLampert <lamperta@gmail.com>
2015-02-03 20:50:54 -0700

    capitalize the first letter of each paragraph

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-var items = ['Boulder', 'rolfing', 'Liquor Mart', 'The Spot', 'flatiron', 'bouldering', 'Avery', 'up the canyon', 'Bolder Boulder', 'Bodarging', 'sit on the grass', 'organic', 'ganja', '420', 'GMO-free', 'triathlon', 'marathon', 'ironman', 'pearl street mall', '29th street', 'b2b', 'microbreweries', 'homebrowing', 'b2b home grow', 'indica', 'sativa', 'home grown b2b', 'hybrid', 'percalator', 'carborator', 'carb', 'reiki', 'reiki master', 'naropa', 'dragonfly', 'cosmos', 'abo\'s', 'illegal pete\'s', 'smelly deli', 'buffalos', 'CU Buffs', 'mountain sun', 'the sun', 'the southern sun', 'under the sun', 'conor\'s', 'TechStars', 'Tesla', 'Boulder Baked', 'ncar', 'mount sanitas', 'drive up to Ned', 'tubing', 'canyoning', 'Chautauqua', 'eTown Hall', 'The Fox', 'Boulder Theater', 'dead leaf', 'Naked Pumpkin Run', 'the cruise', 'thursday night cruise', 'banjo billy\'s', 'yoga', 'tai chi', 'goss gross', 'the nexus', 'Arapahoe', 'Baseline', 'North Boulder', 'Table Mesa', 'Table Mesa Park N Ride', 'The Diagonal', 'Spartan Race', 'balls to the walls', 'hemp', 'gluten free', 'Snarf\'s', 'bike lanes', 'farmer\'s market', 'Wonderland Lake', 'Coconut oil is amazing'
+var items = ['boulder', 'rolfing', 'Liquor Mart', 'The Spot', 'flatiron', 'bouldering', 'Avery', 'up the canyon', 'Bolder Boulder', 'Bodarging', 'sit on the grass', 'organic', 'ganja', '420', 'GMO-free', 'triathlon', 'marathon', 'ironman', 'pearl street mall', '29th street', 'b2b', 'microbreweries', 'homebrowing', 'b2b home grow', 'indica', 'sativa', 'home grown b2b', 'hybrid', 'percalator', 'carborator', 'carb', 'reiki', 'reiki master', 'naropa', 'dragonfly', 'cosmos', 'abo\'s', 'illegal pete\'s', 'smelly deli', 'buffalos', 'CU Buffs', 'mountain sun', 'the sun', 'the southern sun', 'under the sun', 'conor\'s', 'TechStars', 'Tesla', 'Boulder Baked', 'ncar', 'mount sanitas', 'drive up to Ned', 'tubing', 'canyoning', 'Chautauqua', 'eTown Hall', 'The Fox', 'Boulder Theater', 'dead leaf', 'Naked Pumpkin Run', 'the cruise', 'thursday night cruise', 'banjo billy\'s', 'yoga', 'tai chi', 'goss gross', 'the nexus', 'Arapahoe', 'Baseline', 'North Boulder', 'Table Mesa', 'Table Mesa Park N Ride', 'The Diagonal', 'Spartan Race', 'balls to the walls', 'hemp', 'gluten free', 'Snarf\'s', 'bike lanes', 'farmer\'s market', 'Wonderland Lake', 'Coconut oil is amazing'
 
 ];
 
@@ -29,7 +29,11 @@ var dump = $('.dump');
 
 // var tempScrollTop = $(window).scrollTop();
 
+
+
 $('.show-results-btn').click(function(){
+
+
 	event.preventDefault();
 	// clear the DOM on click
 	dump.empty()
@@ -37,16 +41,27 @@ $('.show-results-btn').click(function(){
 
 	// Add a comma to all items
 	var withComma = items.map(function(str, index) {
+
 		if (index % 4 === 0) {
 			return str + ', ';
 		} else {
 			return str + ' ';
 		}
 	});
+	var firstItem = withComma[0];
 
 	// paragraphCount times, shuffle the 
 	_.times(paragraphCount, function(){
 		var shuffledWithComma = _.shuffle(withComma);
+		var firstLetter = shuffledWithComma[0][0];
+		var firstLetterCapped = firstLetter.toUpperCase();
+		var allButFirstLength = shuffledWithComma[0].length - 1;
+		var allButFirst = shuffledWithComma[0].substr(1, allButFirst);
+		shuffledWithComma[0] = firstLetterCapped + allButFirst;
+		
+		// adds period, but also a space
+		// also, there is sometimes a comma, need to ensure that doesn't happen
+		shuffledWithComma.push('.');
 		// debugger;
 		// adding an opening div before the array
 		shuffledWithComma.unshift('<div class="dump-item"' + '<br>' + '<br>');
